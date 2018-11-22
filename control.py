0e03cf1cad14e91de3c597da89476c94d13da2ac
theRaznet <Raznet@users.noreply.github.com>
2018-11-22 17:06:32 -0300

    Create control.py
    Control

---
+++ control.py
@@ -0,0 +1,24 @@
+def factura(m):
+
+
+def boleta(m):
+
+
+n = input("Indique la cantidad de documentos a ingresar: ")
+a = 0
+c = 0
+
+for i in range (1,n):
+    print("Ingreso de docmento %s" % i)
+    b = int(input("Monto del documento: "))
+    inpuesto = (inpuesto + b)
+    tipo = input("Tupo de documento. Boleta o factura")
+    if (tipo = "factura"):
+
+
+
+for i in range (0,50):
+    print("=" end="")
+    
+print("Total de inpuestos a pagar: %s" % b)
+print("Total de ingresos: %s" % c)
